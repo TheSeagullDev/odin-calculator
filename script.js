f07511abbe97611500c86f1b610543c2c7834ece
TheSeagullDev <67653706+TheSeagullDev@users.noreply.github.com>
2024-08-04 13:55:47 -0400

    Make calculator basic logic

--- script.js
+++ script.js
@@ -20,6 +20,8 @@ function divide(a, b)
 
 function operate(a, b, operand)
 {
+    a = parseInt(a);
+    b = parseInt(b);
     switch (operand)
     {
         case "add":
@@ -35,12 +37,62 @@ function operate(a, b, operand)
     }
 }
 
+const numbers = "0123456789"
 
-
-let first = null;
-let second = null;
-let operand = null;
+let first = "";
+let second = "";
+let operand = "";
 const display = document.querySelector("#display");
 
 
+function updateDisplay(value)
+{
+    if(value === "clear")
+    {
+        clearDisplay()
+    }
+    else if(!first && numbers.includes(value))
+    {
+        first = value;
+        display.textContent = first;
+    }
+    else if(!operand && numbers.includes(value))
+    {
+        first += String(value);
+        display.textContent = first;
+    }
+    else if(value === "equals" && first && second && operand)
+    {
+        display.textContent = operate(first, second, operand);
+        first = display.textContent;
+        second = "";
+        operand = "";
+    }
+    else if(!numbers.includes(value) && first)
+    {
+        operand = value;
+    }
+    else if(operand && numbers.includes(value) && !second)
+    {
+        second = value;
+        display.textContent = value;
+    }
+    else if(operand && numbers.includes(value) && second)
+    {
+        second += String(value);
+        display.textContent = second;
+    }
+}
 
+function clearDisplay()
+{
+    first = "";
+    second = "";
+    operand = "";
+    display.textContent = 0;
+}
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => button.addEventListener("click", () => {
+    updateDisplay(button.id);
+}))
